c244ad83f7d95019768e7e1686ab81aa0a2d8fb1
SAROJ <080bct075.saroj@pcampus.edu.np>
2025-02-15 22:41:35 +0545

    feat: add dividerline and titleText

--- src/UI/UI.cpp
+++ src/UI/UI.cpp
@@ -1,15 +1,50 @@
 #include "UI.hpp"
 
-bool UI::initialize(sf::RenderWindow& window) {
+#include <iostream>
+#include <ostream>
+
+
+sf::Vector2u UI::windowWidth;
+
+bool UI::initialize(sf::RenderWindow& window)
+{
+    windowWidth = window.getSize();
+    std::cout << windowWidth.x << std::endl;
     return ImGui::SFML::Init(window);
 }
 
-void UI::render() {
+void UI::render(sf::RenderWindow& window)
+{
     ImGui::Begin("Hello, world!");
     ImGui::Button("Look at this pretty button");
     ImGui::End();
+
+    // Divider line
+    float dividerLinePositionX = static_cast<float>(0.25) * static_cast<float>(windowWidth.x);
+    sf::RectangleShape dividerLine({1.f, static_cast<float>(windowWidth.y)});
+    dividerLine.setPosition({dividerLinePositionX, 0.f});
+
+    // Title text container
+    sf::RectangleShape titleTextContainer({460.f, 40.f});
+    titleTextContainer.setPosition({(dividerLinePositionX - titleTextContainer.getLocalBounds().size.x) / 2.f, 20.f});
+    titleTextContainer.setFillColor(sf::Color(255, 255, 255, 0));
+    titleTextContainer.setOutlineThickness(2.f);
+    titleTextContainer.setOutlineColor(sf::Color::Green);
+    titleTextContainer.setOutlineColor(sf::Color(250, 150, 100));
+
+    // Title text
+    sf::Text titleText(Core::font);
+    titleText.setString("Transformation Visualizer");
+    titleText.setCharacterSize(24);
+    titleText.setPosition({(dividerLinePositionX - titleText.getLocalBounds().size.x) / 2, 25.f});
+    titleText.setFillColor(sf::Color::White);
+
+    window.draw(dividerLine);
+    window.draw(titleTextContainer);
+    window.draw(titleText);
 }
 
-void UI::shutdown() {
+void UI::shutdown()
+{
     ImGui::SFML::Shutdown();
 }
--- src/UI/UI.hpp
+++ src/UI/UI.hpp
@@ -4,12 +4,16 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <Core/Core.hpp>
+#include "SFML/Graphics/RectangleShape.hpp"
+#include "SFML/Graphics/Text.hpp"
 
 class UI {
 public:
     static bool initialize(sf::RenderWindow& window);
-    static void render();
+    static void render(sf::RenderWindow& window);
     static void shutdown();
+    static sf::Vector2u windowWidth;
 };
 
 #endif // UI_HPP
