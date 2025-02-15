6dbec3b1fa24bd88ca602a427945bdecbe16cf7a
Sunil Lamichhane <080bct090.sunil@pcampus.edu.np>
2025-02-15 22:36:22 +0545

    feat: add options and button menu at the left most side

--- src/UI/UI.cpp
+++ src/UI/UI.cpp
@@ -1,13 +1,67 @@
 #include "UI.hpp"
-
+#include <iostream>
 bool UI::initialize(sf::RenderWindow& window) {
     return ImGui::SFML::Init(window);
 }
 
 void UI::render() {
-    ImGui::Begin("Hello, world!");
-    ImGui::Button("Look at this pretty button");
+
+
+    // now for buttons to add shape or clear canvas
+    ImGui::SetNextWindowPos(ImVec2(30,100),ImGuiCond_Always);
+    ImGui::SetNextWindowSize(ImVec2(200,100));
+
+    ImGui::Begin("Options ", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
+    ImGui::SetWindowFontScale(1.5f);
+    if (ImGui::Button("Add Shape")) {
+        std::cout << "Add" << std::endl;
+    }
+    if (ImGui::Button("Clear Canvas")) {
+        std::cout << "Option 2 Clicked!" << std::endl;
+    }
+
     ImGui::End();
+
+    //now for options for 2d transformation
+    ImGui::SetNextWindowPos(ImVec2(30,250),ImGuiCond_Always);
+    ImGui::SetNextWindowSize(ImVec2(200,250));
+
+    ImGui::Begin("Transformations", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
+    ImGui::SetWindowFontScale(1.5f);
+    if (ImGui::Button("Scale")) {
+        std::cout << "Add" << std::endl;
+    }
+    if (ImGui::Button("Translate")) {
+        std::cout << "Option 2 Clicked!" << std::endl;
+    }
+    if (ImGui::Button("Rotate")) {
+        std::cout << "Add" << std::endl;
+    }
+    if (ImGui::Button("Reflect")) {
+        std::cout << "Option 2 Clicked!" << std::endl;
+    }
+    if (ImGui::Button("Shear")) {
+        std::cout << "Add" << std::endl;
+    }
+
+
+    ImGui::End();
+
+    //modal window for options
+    static bool showLastWindow = true;
+
+    // Conditionally render the last window
+    if (showLastWindow) {
+        ImGui::SetNextWindowPos(ImVec2(30, 500), ImGuiCond_Always);
+        ImGui::SetNextWindowSize(ImVec2(400, 350));
+
+        ImGui::Begin("Valuesss", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
+        ImGui::SetWindowFontScale(1.5f);
+
+        // Your content here
+
+        ImGui::End();
+    }
 }
 
 void UI::shutdown() {
